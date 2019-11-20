1efa4c60e477164c379d2467933e463dba1a3227
gaoyunpeng <gaoyunpeng1982@gmail.com>
2019-11-20 18:11:05 +0900

    新建工程

---
+++ EasyToDo/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyToDo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ EasyToDo/Login.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace EasyToDo
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
